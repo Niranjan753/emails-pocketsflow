0d6989879ef527380504796707e13eeea5e5c036
Niranjan753 <niranjanr753@gmail.com>
2026-01-22 13:13:51 +0530

    shadcn init

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Hello for the emails.
-        </h1>
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-center">
+        Pocketsflow is a powerful, yet easy to use platform <br /> that gives you the tools to run and scale an online business <br /> from growing audiences, sending emails, or selling products, all in one place.
+      </h1>
     </div>
   );
 }
